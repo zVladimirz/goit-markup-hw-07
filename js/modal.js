22a879788f98a922434a5419734ca66e710ebd5b
zVladimirz <tzxdlo@gmail.com>
2022-01-21 22:06:36 +0200

    start

---
+++ js/modal.js
@@ -0,0 +1,29 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+
+(() => {
+  document
+    .querySelector('.form-register')
+    .addEventListener('submit', e => {
+      e.preventDefault();
+
+      new FormData(e.currentTarget).forEach((value, name) =>
+        console.log(`${name}: ${value}`),
+      );
+
+      e.currentTarget.reset();
+    });
+})();
